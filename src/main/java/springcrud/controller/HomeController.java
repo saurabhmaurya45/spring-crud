c62b7c71675e907b0de732d377015eb1f579d8c1
saurabhmaurya <tgtsaurabhmaurya45@gmail.com>
2024-03-18 14:48:47 +0530

    configuration completed

---
+++ src/main/java/springcrud/controller/HomeController.java
@@ -0,0 +1,14 @@
+package springcrud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	@RequestMapping("home")
+	public String index() {
+		return "index";
+	}
+	
+}
